1823baf8ed00352499a1e085abb3bb0df43d2277
Hilmi <hilmi.azman@srglobal.com>
2020-09-16 00:42:59 +0800

    Setup app,js, routes.js and create sample components

---
+++ resources/js/routes.js
@@ -0,0 +1,22 @@
+export default {
+
+
+    mode: 'history',
+    routes: [
+
+        {
+            path:'/',
+            component: Home
+        },
+
+
+        {
+            path:'/about',
+            component: About
+        },
+
+
+    ]
+
+
+}
