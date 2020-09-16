b59aa552c10e153cc6e913fda50af631c0d1d816
Hilmi <hilmi.azman@srglobal.com>
2020-09-16 22:13:23 +0800

    Setup dynamic component

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,5 +1,7 @@
 import Home from './components/Home';
-import About from './components/About';
+//import About from './components/About';
+
+let About = () => import(/* webpackChunkName: "Components/About" */ './components/About');
 
 export default {
 
